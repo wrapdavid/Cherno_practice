a0d8b2312831b41c48286845410c31307dcbc37f
etdog0910@gmail.com <etdog0910@gmail.com>
2019-11-25 14:59:39 +0800

    learning template

---
+++ Project1/src/Main65.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+template<typename T,int size>
+class Array {
+private:
+	T m_Array[size];
+public:
+	int GetSize() const{ return size; }
+
+};
+template<typename T>
+void Print(T value) {
+	std::cout << value << std::endl;
+}
+
+int main() {
+	Print("\63");
+	Array<int,5> array;
+	std::cout << array.GetSize() << std::endl;
+	std::cin.get();
+}

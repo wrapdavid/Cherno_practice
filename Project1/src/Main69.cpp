0a12bf357916056db052236c9794d1672ae22401
etdog0910@gmail.com <etdog0910@gmail.com>
2019-12-09 19:35:51 +0800

    The auto keyword in C++_56-2

--- Project1/src/Main69.cpp
+++ Project1/src/Main69.cpp
@@ -1,14 +1,39 @@
 #include<iostream>
-
+#include<vector>
+#include<iterator>
+#include<unordered_map>
 const char* GetName() {
 	return "david";
 }
 
+class Device{};
 
+class DeviceManager {
+private:
+	std::unordered_map<std::string, std::vector<Device*>> m_Devices;
+
+
+public:
+	const std::unordered_map<std::string, std::vector<Device*>>& GetDevices() const {
+		return m_Devices;
+	}
+};
 int main() {
-	//auto name = Name();
-	std::string name = GetName();
-	int size = name.size();
-	std::cout << size << std::endl;
+	std::vector<std::string> strings;
+	strings.push_back("apple");
+	strings.push_back("orange");
+	
+	//std::vector<std::string>::iterator
+	for (auto it = strings.begin(); it != strings.end(); it++) {
+		std::cout << *it << std::endl;
+	}
+
+	using DeviceMap =  std::unordered_map<std::string, std::vector<Device*>>;
+	typedef  std::unordered_map<std::string, std::vector<Device*>> devicemap;
+
+	DeviceManager dm;
+	
+	const auto& devices = dm.GetDevices();
+
 	std::cin.get();
 }

5d2534b82beb907dbdf166e7a4cd63b91264fcd2
etdog0910@gmail.com <etdog0910@gmail.com>
2019-12-18 19:42:15 +0800

    Multidimensional Arrays in C++ (2D arrays)_64-3

--- Project1/src/Main83.cpp
+++ Project1/src/Main83.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 #include<string>
-
+#include<chrono>
+struct Timer {
+	std::chrono::steady_clock::time_point start, end;
+	std::chrono::duration<float> duration;
+	Timer() {
+		start = std::chrono::high_resolution_clock::now();
+	}
+	~Timer() {
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		float ms = duration.count() * 1000.0f;
+		std::cout << "it took: " << ms << "ms" << std::endl;
+	}
+};
 int main() {
+	{
+		Timer time;
 	int** array = new int* [5];
 	for (int y = 0; y < 5; y++) {
 
@@ -13,7 +28,10 @@ int main() {
 		}
 	}
 
+	}
 
+	{
+		Timer time;
 	int* a2d = new int[5 * 5];
 	for (int y = 0; y < 5; y++) {
 
@@ -22,6 +40,7 @@ int main() {
 			a2d[x + y * 5] = 2;
 
 		}
+	}
 	}
 	std::cin.get();
 }
